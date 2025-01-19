3a3397c6d050d01903dfb31e09cf485ff6ed02fc
brolando94 <47872050+brolando94@users.noreply.github.com>
2025-01-19 13:00:15 -0600

    sends message in local time

--- main.py
+++ main.py
@@ -14,12 +14,6 @@ options.headless = False
 driver = Firefox()
 driver.set_window_size(1920, 1080)
 
-local_timezone_list = str(datetime.now().astimezone().tzinfo).split()
-local_timezone = ''
-for word in local_timezone_list:
-    local_timezone += word[0]
-
-
 team_mapping = {
     'rams': '<:rams:1316594568690536500>',
     '49ers': '<:49ers:1316594555969339455>',
@@ -60,30 +54,73 @@ load_dotenv(r'vars.env')
 
 webhook_url = env.get('webhook_url')
 
+# grab url variables
 current_year = env.get('year')
+current_season = env.get('season')
+current_week = env.get('week')
+
+# check to make sure they exist
 if current_year is None:
     current_year = str(date.today().year)
     set_key(r'vars.env', 'year', f'{current_year}')
 else:
     current_year = str(current_year)
 
-current_week = env.get('week')
-if current_week is None:
-    current_week = '1'
-    set_key(r'vars.env', 'week', f'{current_week}')
+if current_season is None:
+    current_season = '1'
+    set_key(r'vars.env', 'season', '1')
 else:
+    current_season = str(current_season)
+
+if current_week is None:
+    current_week = '2'
+    set_key(r'vars.env', 'week', '2')
+else:
+    # move to the next week
     current_week = int(current_week) + 1
     current_week = str(current_week)
+    set_key(r'vars.env', 'week', f'{current_week}')
 
-set_key(r'vars.env', 'week', f'{current_week}')
+# switch to regular season
+if current_week == '5' and current_season == '1':
+    current_season = '2'
+    set_key(r'vars.env', 'season', '2')
+    current_week = '1'
+    set_key(r'vars.env', 'week', '1')
+# switch for playoffs
+elif current_week == '19' and current_season == '2':
+    current_season = '3'
+    set_key(r'vars.env', 'season', '3')
+    current_week = '1'
+    set_key(r'vars.env', 'week', '1')
+
+
 # request the page
-driver.get(url=f'https://www.espn.com/nfl/scoreboard/_/week/{current_week}/year/{current_year}/seasontype/2')
+url = f'https://www.espn.com/nfl/scoreboard/_/week/{current_week}/year/{current_year}/seasontype/{current_season}'
+driver.get(url=url)
 sleep(3)
 # parse the game data
 game_dict = scraper.parse_game_details(driver)
 
-# send the staring message if there are games
-starting_msg = f"Week {current_week} NFL Pick'ems.\nVote using team emoji's on each game message"
+# translate the season number and week into the staring message
+match current_season:
+    case '1':
+        starting_msg = f'Preseason Week {current_week}\nVote using team emoji\'s on each game message'
+    case '2':
+        starting_msg = f'Week {current_week}\nVote using team emoji\'s on each game message'
+    case '3':
+        match current_week:
+            case '1':
+                starting_msg = 'Playoff Wild Card\nVote using team emoji\'s on each game message'
+            case '2':
+                starting_msg = 'Playoff Divisional\nVote using team emoji\'s on each game message'
+            case '3':
+                starting_msg = 'Conference Championships\nVote using team emoji on game message'
+            case '4':
+                starting_msg = 'Pro Bowl\nVote using team emoji on game message'
+            case '5':
+                starting_msg = 'Super Bowl\nVote using team emoji on game message'
+
 if len(game_dict) > 0:
     print(operations.send(message=starting_msg, webhook_url=webhook_url))
 
@@ -91,7 +128,7 @@ if len(game_dict) > 0:
 game_days = game_dict.keys()
 for game_day in game_days:
     # send the message for the date
-    print(operations.send(message=f"# {game_day}", webhook_url=webhook_url))
+    # print(operations.send(message=f"# {game_day}", webhook_url=webhook_url))
     sleep(1)
     # loop the game data and build the message
     for game in game_dict[game_day]:
@@ -99,11 +136,24 @@ for game_day in game_days:
         home_emoji = team_mapping[home.lower()]
         away = game['away_team']
         away_emoji = team_mapping[away.lower()]
+        # convert date and time to unix timestamp
+        date_to_convert = f"{game_day} {game['game_time']}"
+        # Parse the date string into a datetime object
+        dt_object = datetime.strptime(date_to_convert, "%A, %B %d, %Y %I:%M %p")
+        # Convert the datetime object to a Unix timestamp
+        game_timestamp = int(dt_object.timestamp())
         game_msg = f"**{away}** {away_emoji} @ **{home}** {home_emoji}" \
-                   f"\nGame Info: {game['game_time']} {local_timezone} - {game['channel']}"
+                   f"\nGame Time: <t:{game_timestamp}:t>" \
+                   f"\nChannel: {game['channel']}"
 
         # send message
         print(operations.send(message=game_msg, webhook_url=webhook_url))
         sleep(1)
 
 driver.close()
+
+# need to end the game and set it up for next year
+if current_season == '3' and current_week == '5':
+    set_key(r'vars.env', 'year', f'{int(current_year) + 1}')
+    set_key(r'vars.env', 'week', '2')
+    set_key(r'vars.env', 'season', '1')
